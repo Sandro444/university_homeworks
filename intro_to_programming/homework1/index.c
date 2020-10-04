d78dd85bce46123147d4a20d3ce1224a2a6bc4c4
Sandro444 <tsereteli.sandro0401@gmail.com>
2020-10-04 17:02:12 +0400

    Create index.c

---
+++ intro_to_programming/homework1/index.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main() {
+   int phoneNumber = 599400500;
+   float height = 2.2;
+   int favouriteNumber = 7;
+   printf("Phone Number: %d\nHeight: %.2f meter\nFavourite Number: %d", phoneNumber, height, favouriteNumber);
+   return 0;
+}
